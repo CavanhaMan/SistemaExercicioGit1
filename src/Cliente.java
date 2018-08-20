004f1434c4e1e9ec3eb710b47d9ab5777127169c
CavanhaMan <cavanhaman@yahoo.com.br>
2018-08-19 22:19:44 -0300

    Inclusao do campo id

--- src/Cliente.java
+++ src/Cliente.java
@@ -10,7 +10,8 @@ import java.util.Scanner;
 public class Cliente {
 
 	private String nome;
-
+	private int id;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
