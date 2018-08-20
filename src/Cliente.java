461e447cd202515681c7ed64594b6ac324eeae63
CavanhaMan <cavanhaman@yahoo.com.br>
2018-08-19 22:11:22 -0300

    Inclusao da classe Cliente

---
+++ src/Cliente.java
@@ -0,0 +1,15 @@
+/*
++---------------------+
+| Rodrigo  CavanhaMan |
+|        IFTM         |
+|Engenharia de Sofware|
++---------------------+
+*/
+public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
