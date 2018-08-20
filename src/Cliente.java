b03d281549323a7a60cf10ec2d37a73552da4e9b
CavanhaMan <cavanhaman@yahoo.com.br>
2018-08-19 22:15:23 -0300

    Adicao do campo nome

--- src/Cliente.java
+++ src/Cliente.java
@@ -9,10 +9,11 @@ import java.util.Scanner;
 
 public class Cliente {
 
+	private String nome;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String nome;
 		
 		sc.close();
 	}
