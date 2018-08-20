0058d82f03c6ad20f07c5ff8d124ebbd1e673520
CavanhaMan <cavanhaman@yahoo.com.br>
2018-08-19 22:43:35 -0300

    Inclusao da classe connta

---
+++ src/model/domain/Conta.java
@@ -0,0 +1,12 @@
+package model.domain;
+/*
++---------------------+
+| Rodrigo  CavanhaMan |
+|        IFTM         |
+|Engenharia de Sofware|
++---------------------+
+*/
+
+public class Conta {
+
+}
